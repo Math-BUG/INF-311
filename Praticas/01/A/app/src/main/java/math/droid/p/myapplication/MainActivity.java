4748c298ed6c462294ed44bf7f43283e6a742d27
Math-BUG <matheus.antony.dev@gmail.com>
2025-03-29 03:57:20 -0300

    Primeira parte feita!

---
+++ Praticas/01/A/app/src/main/java/math/droid/p/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package math.droid.p.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText n1;
+    private EditText n2;
+    private TextView res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        n1 = findViewById(R.id.n1);
+        n2 = findViewById(R.id.n2);
+        res = findViewById(R.id.res);
+    }
+
+    public void somar(View view) {
+        int v1 = Integer.parseInt(n1.getText().toString());
+        int v2 = Integer.parseInt(n2.getText().toString());
+        res.setText(String.valueOf(v1+v2));
+    }
+
+    public void subtrair(View view) {
+        int v1 = Integer.parseInt(n1.getText().toString());
+        int v2 = Integer.parseInt(n2.getText().toString());
+        res.setText(String.valueOf(v1-v2));
+    }
+
+    public void multiplicar(View view) {
+        int v1 = Integer.parseInt(n1.getText().toString());
+        int v2 = Integer.parseInt(n2.getText().toString());
+        res.setText(String.valueOf(v1*v2));
+    }
+
+    public void dividir(View view) {
+        int v1 = Integer.parseInt(n1.getText().toString());
+        int v2 = Integer.parseInt(n2.getText().toString());
+        res.setText(String.valueOf((float)v1/v2));
+    }
+}
